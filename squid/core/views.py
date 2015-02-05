98ef73c8a246ec1a983bf9b6fabe5e596f0895ad
Stephen Donaldson <sirnarwhal@gmail.com>
2015-02-04 22:17:55 -0500

    Creating views.py.

--- squid/core/views.py
+++ squid/core/views.py
@@ -1,3 +1,29 @@
+# Create your views here.
+from decimal import *
+
+from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpResponseBadRequest
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from .models import Member, Event, Venue, MemberRSVP
+
+class MemberView(ListView):
+	model = Member
+	template_name = "members.html"
+
+class EventView(ListView):
+	model = Event
+	template_name = "events.html"
+
+class VenueView(ListView):
+	model = Venue
+	template_name = "venues.html"
+
+class MemberRSVPView(ListView):
+	model = MemberRSVP
+	template_name = "rsvps.html"
