018bf0a32c184befeb4b21e83157e67774e6d239
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-02-04 20:28:47 -0500

    python 2 to 3 model example

--- squid/core/models.py
+++ squid/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
+from django.utils.encoding import python_2_unicode_compatible
 
 
 class Member(TimeStampedModel):
@@ -33,6 +34,7 @@ class Venue(TimeStampedModel):
     latitude = models.CharField(max_length=128)
 
 
+@python_2_unicode_compatible
 class MemberRSVP(TimeStampedModel):
     """
     Django NYC RSVPs to events by members
@@ -41,3 +43,9 @@ class MemberRSVP(TimeStampedModel):
     member = models.ForeignKey('core.Member', related_name="events")
     event = models.ForeignKey('core.Event', related_name="rsvps")
     join_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "{member} RSVP to {event}".format(
+            member=self.member.name,
+            event=self.event.title
+            )
