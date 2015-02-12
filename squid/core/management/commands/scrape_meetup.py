27aee2c4c41d8ff948eb61ec43241f48de872443
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-02-11 20:35:13 -0500

    adding initial scrape meetup

--- squid/core/management/commands/scrape_meetup.py
+++ squid/core/management/commands/scrape_meetup.py
@@ -10,10 +10,21 @@ from django.conf import settings
 class Command(BaseCommand):
     def handle(self, *args, **options):
         import ipdb; ipdb.set_trace()
+        API_BASE = 'https://api.meetup.com/'
+
+        def get_api_url(endpoint):
+            return "{base}{endpoint}".format(
+                base=API_BASE,
+                endpoint=endpoint
+                )
+
         api_key = settings.MEETUP_API_KEY
         api_params = {'sign':'true', 'key':api_key}
 
         group_params = api_params
         group_params['group_urlname'] = 'django-nyc'
+        group_url = get_api_url('2/groups')
 
-        django_nyc = requests.get('https://api.meetup.com/2/groups', group_params)
+        django_nyc = requests.get(group_url, params=group_params)
+
+
