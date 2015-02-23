bd48ba82907690f4c1848e79da962e138dd94db1
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-02-23 17:48:44 -0500

    adding some color to members

--- squid/core/forms.py
+++ squid/core/forms.py
@@ -4,6 +4,10 @@ from .models import MemberRSVP
 
 class EventAttendeeForm(forms.ModelForm):
     id = forms.IntegerField(widget=forms.HiddenInput)
+    worked_on = forms.CharField(widget=forms.Textarea(attrs={
+        'cols': '35',
+        'rows': '5'
+        }))
 
     class Meta:
         model = MemberRSVP
