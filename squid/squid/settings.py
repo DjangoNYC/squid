8445404cb066a8eb66cbf3a2d2233c8d35bf67f8
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-02-24 15:21:54 -0500

    adding postgresql database info

--- squid/squid/settings.py
+++ squid/squid/settings.py
@@ -72,14 +72,26 @@ WSGI_APPLICATION = 'squid.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'squid',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'squid',
+        'PASSWORD': 'squid',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
